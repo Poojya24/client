dcc7f979b1e89ed5b38223eac0734a9f6197b632
poojya <poojya@poojyas-MacBook-Air.local>
2026-03-24 15:14:48 +0530

    updated API to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_API_URL || "https://server-1-a19b.onrender.com/api";
 
 const readToken = () => localStorage.getItem("token") || "";
 
